8d886f3b3b73e040d0ffa70ee523c92bc1752a1d
Tsankur <hadrien.bruley@gmail.com>
2014-11-30 17:35:13 +0100

    passage du menu principal a l'éditeur

---
+++ projet/Assets/script/main_menu/menu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void changeScene(string scene_name)
+    {
+        Application.LoadLevel(scene_name);
+    }
+}
