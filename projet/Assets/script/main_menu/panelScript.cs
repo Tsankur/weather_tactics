e4d7fce8a2592c73dc28e97ace57c390ab72a919
Tsankur <hadrien.bruley@gmail.com>
2014-12-01 21:57:09 +0100

    ajout resize si la largeur de l'écran est trop grande

---
+++ projet/Assets/script/main_menu/panelScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class panelScript : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        if(Screen.currentResolution.width > 480);
+        RectTransform rectTransform = GetComponent<RectTransform>();
+        rectTransform.anchorMin = new Vector2(0.5f, 0.5f);
+        rectTransform.anchorMax = new Vector2(0.5f, 0.5f);
+        rectTransform.sizeDelta = new Vector2(526, 760); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
