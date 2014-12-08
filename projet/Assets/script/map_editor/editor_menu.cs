ee41e85f5e4cc6d7c526caae8ca5aee44d71a167
Tsankur <hadrien.bruley@gmail.com>
2014-12-08 23:20:18 +0100

    début ajout du menu de l'éditeur de niveaux avec seulement les type de terrain et choix au click

---
+++ projet/Assets/script/map_editor/editor_menu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System.Collections;
+
+public class editor_menu : MonoBehaviour
+{
+    public Image selectedTool;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void changeSelected(Image clickedButton)
+    {
+        selectedTool.color = clickedButton.color;
+    }
+}
