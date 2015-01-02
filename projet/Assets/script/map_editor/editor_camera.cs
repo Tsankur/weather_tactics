8c27d2e1250b00825021a08312d84ebf9abc9630
Tsankur <hadrien.bruley@gmail.com>
2015-01-02 19:09:28 +0100

    ajout édition des case et mouvement + restriction de la caméra dans l'éditeur

---
+++ projet/Assets/script/map_editor/editor_camera.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class editor_camera : MonoBehaviour
+{
+    private Vector2 m_vMaxPosition = new Vector3(0, 0);
+    private bool m_bMoving = false;
+    private Vector3 m_vLastMousePosition;
+    private float m_fLastWheelValue = 0;
+    private float m_fZoom = 150;
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetMouseButtonDown(1))
+        {
+            m_vLastMousePosition = Input.mousePosition;
+            m_bMoving = true;
+        }
+        float fMouseWheelDelta = Input.GetAxis("Mouse ScrollWheel");
+        if (fMouseWheelDelta != 0.0f)
+        {
+            m_fZoom -= fMouseWheelDelta * 50;
+            if (m_fZoom > 300)
+            {
+                m_fZoom = 300;
+            }
+            if (m_fZoom < 50)
+            {
+                m_fZoom = 50;
+            }
+            transform.position = new Vector3(transform.position.x, transform.position.y, -m_fZoom);
+        }
+        if (m_bMoving)
+        {
+            transform.position += (m_vLastMousePosition - Input.mousePosition) / (800.0f / m_fZoom);
+            float newX = transform.position.x, newY = transform.position.y;
+            if (transform.position.x > m_vMaxPosition.x)
+            {
+                newX = m_vMaxPosition.x;
+            }
+            if (transform.position.y > m_vMaxPosition.y)
+            {
+                newY = m_vMaxPosition.y;
+            }
+            if (transform.position.x < 0)
+            {
+                newX = 0;
+            }
+            if (transform.position.y < 0)
+            {
+                newY = 0;
+            }
+            transform.position = new Vector3(newX, newY, -m_fZoom);
+            m_vLastMousePosition = Input.mousePosition;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            m_bMoving = false;
+        }
+	}
+    public void SetMaxPosition(Vector2 _vMaxPosition)
+    {
+        m_vMaxPosition = _vMaxPosition;
+        if (transform.position.x > m_vMaxPosition.x)
+        {
+            transform.position = new Vector3(m_vMaxPosition.x, transform.position.y, -m_fZoom);
+        }
+        if (transform.position.y > m_vMaxPosition.y)
+        {
+            transform.position = new Vector3(transform.position.x, m_vMaxPosition.y, -m_fZoom);
+        }
+    }
+}
--- projet/Assets/script/map_editor/editor_menu.cs
+++ projet/Assets/script/map_editor/editor_menu.cs
@@ -8,8 +8,13 @@ public class editor_menu : MonoBehaviour
     public Image selectedTool;
     public GameObject GridHolder;
     public GameObject GridElement;
-    private int m_iWidth = 10;
-    private int m_iHeight = 10;
+    private int m_iWidth = 0;
+    private int m_iHeight = 0;
+    private int m_iNewWidth = 10;
+    private int m_iNewHeight = 10;
+    private GameObject[,] GridElements;
+    public Camera m_MainCamera;
+    public Material m_SelectedMaterial;
 	// Use this for initialization
 	void Start ()
     {
@@ -19,35 +24,65 @@ public class editor_menu : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfos;
+            if (Physics.Raycast(ray,out hitInfos))
+            {
+                hitInfos.collider.gameObject.GetComponent<Renderer>().material = m_SelectedMaterial;
+            }
+        }
 	}
     public void changeSelected(Image clickedButton)
     {
         selectedTool.color = clickedButton.color;
     }
-    private void updateGrid()
+    public void changeSelectedMaterial(Material _material)
     {
-        foreach (Transform child in GridHolder.transform)
+        m_SelectedMaterial = _material;
+    }
+    public void updateGrid()
+    {
+        GameObject[,] oldGridElements = GridElements;
+        /*foreach (Transform child in GridHolder.transform)
         {
             GameObject.Destroy(child.gameObject);
-        }
-        float iStartX = -m_iWidth / 2.0f + 0.5f;
-        float iStartY = -m_iHeight / 2.0f + 0.5f;
-        for(int i = 0; i < m_iWidth; i++)
+        }*/
+        GridElements = new GameObject[m_iNewWidth, m_iNewHeight];
+        int maxWidth = Mathf.Max(m_iWidth, m_iNewWidth);
+        int maxHeight = Mathf.Max(m_iHeight, m_iNewHeight);
+        for (int i = 0; i < maxWidth; i++)
         {
-            for (int j = 0; j < m_iHeight; j++)
+            for (int j = 0; j < maxHeight; j++)
             {
-                GameObject newGridElement = (GameObject)Instantiate(GridElement, new Vector3((iStartX + i) * 10, (iStartY + j) * 10), Quaternion.identity);
-                newGridElement.transform.SetParent(GridHolder.transform);
+                if (i >= m_iNewWidth || j >= m_iNewHeight)
+                {
+                    GameObject.Destroy(oldGridElements[i, j]);
+                }
+                else if (i >= m_iWidth || j >= m_iHeight)
+                {
+                    GameObject newGridElement = (GameObject)Instantiate(GridElement, new Vector3(i * 10, j * 10), Quaternion.identity);
+                    newGridElement.transform.SetParent(GridHolder.transform);
+                    GridElements[i, j] = newGridElement;
+                }
+                else
+                {
+                    GridElements[i, j] = oldGridElements[i, j];
+                }
             }
         }
+        m_iWidth = m_iNewWidth;
+        m_iHeight = m_iNewHeight;
+        Camera.main.GetComponent<editor_camera>().SetMaxPosition(new Vector2((m_iWidth - 1) * 10, (m_iHeight - 1) * 10));
     }
     public void changeHeight(string _sValue)
     {
         int iHeight = int.Parse('0' + _sValue);
         if(iHeight > 0 && iHeight < 1000)
         {
-            m_iHeight = iHeight;
-            updateGrid();
+            m_iNewHeight = iHeight;
+            //updateGrid();
         }
     }
     public void changeWidth(string _sValue)
@@ -55,8 +90,8 @@ public class editor_menu : MonoBehaviour
         int iWidth = int.Parse('0' + _sValue);
         if (iWidth > 0 && iWidth < 1000)
         {
-            m_iWidth = iWidth;
-            updateGrid();
+            m_iNewWidth = iWidth;
+            //updateGrid();
         }
     }
 }
